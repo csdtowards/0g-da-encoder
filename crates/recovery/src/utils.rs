e2af22d5b23f26f79885f79fb860e7c627d5bcf6
Joel <liuyuanvic@gmail.com>
2024-07-03 16:40:20 +0800

    test

--- crates/recovery/src/utils.rs
+++ crates/recovery/src/utils.rs
@@ -19,7 +19,40 @@ use zg_encoder::{
     raw_unit_to_scalar,
 };
 
-use crate::{poly::Poly, zpoly::COSET_MORE};
+use crate::{data_poly, poly::Poly, zpoly::COSET_MORE};
+
+use std::collections::BTreeMap;
+use zg_encoder::RawData;
+
+pub fn recover_from_da_slice(
+    slices: &BTreeMap<usize, Vec<u8>>,
+) -> Result<Vec<u8>, String> {
+    use data_poly::data_poly;
+    use raw_slice_to_line;
+    use zg_encoder::constants::{Scalar, BLOB_ROW_N};
+
+    let converted_lines: BTreeMap<usize, Vec<Scalar>> = slices
+        .iter()
+        .filter_map(|(idx, raw)| {
+            Some((*idx, raw_slice_to_line(raw.as_slice()).ok()?))
+        })
+        .collect();
+
+    let dropped = slices.len() - converted_lines.len();
+    if dropped > 0 {
+        info!("{:?} lines dropped because of incorrect format", dropped);
+    }
+
+    if converted_lines.len() < BLOB_ROW_N {
+        return Err("Not enough valid lines".to_string());
+    }
+
+    let raw_blob = data_poly(&converted_lines)
+        .map_err(|e| format!("Cannot recover data: {:?}", e))?;
+
+    let raw_data: RawData = raw_blob.try_into()?;
+    Ok(raw_data.as_bytes().to_vec())
+}
 
 pub fn raw_slice_to_line(slice: &[u8]) -> Result<Vec<Scalar>, String> {
     if slice.len() != MAX_BLOB_SIZE {
